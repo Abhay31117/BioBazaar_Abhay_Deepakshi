58e99acfd627b7ca97385ff0a98ce20f1494bff7
Abhay31117 <144879374+Abhay31117@users.noreply.github.com>
2024-11-17 11:10:56 +0530

    hey

---
+++ app/src/main/java/com/example/farmerproducts/splash_screen_activity.kt
@@ -0,0 +1,14 @@
+package com.example.farmerproducts
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class splash_screen_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
